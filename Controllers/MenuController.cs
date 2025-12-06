9dde99cadce53533c08c3e86ac7b97db316102ac
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-06 17:51:50 +0100

    added view for menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using food_menu.Data;
+using food_menu.Models;
+
+namespace food_menu.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
